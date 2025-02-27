75e29cd41bbfa1aa50f428cbce2489b5750e35f5
Marcus Little <mcolelittle@gmail.com>
2025-02-27 09:39:49 -0500

    fix ssr function

--- pages/ssr-page.jsx
+++ pages/ssr-page.jsx
@@ -1,16 +1,15 @@
-export default function Page({ data }) {
-    <>
-    <h1>Check out these dogs</h1>
-    <div>{data}</div>
-    </>
-  }
-   
-  // This gets called on every request
-  export async function getServerSideProps() {
+export async function getServerSideProps() {
     // Fetch data from external API
-    const res = await fetch(`https://dog.ceo/api/breeds/image/random`)
+    const res = await fetch('https://dog.ceo/api/breeds/image/random')
     const data = await res.json()
-   
     // Pass data to the page via props
     return { props: { data } }
+  }
+   
+  export default function Page({ data }) {
+    return (
+      <main>
+        <p>{data}</p>
+      </main>
+    )
   }
